5151152243dc3ee41902aebb1f022334f52d5d9b
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-07-05 10:53:32 +0530

    Backend almost completed, frontend not completed

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 const startApplication = async () => {
 	try {
-		await mongooseConnect(process.env.MONGO_URI);
+		await mongooseConnect(process.env.MONGO_URI_CLOUD);
 		app.listen(PORT, () => {
 			console.log("app is listening on port 5000");
 		});
